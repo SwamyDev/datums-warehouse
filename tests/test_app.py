5a48642844006dc67cef49dc7b3b95c724675b10
SwamyDev <b.raml@gmx.at>
2019-11-24 10:19:23 +0100

    Cover logging configuration

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,11 @@ def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
+
+def test_configure_logging(tmp_path):
+    create_app({
+        'LOG_FILE': str(tmp_path / "test.log"),
+        'LOG_LEVEL': "error",
+    })
+
+    assert (tmp_path / "test.log").exists()
