0630505edf72119ab885e23dd405acf54d2d0df1
SwamyDev <b.raml@gmx.at>
2019-11-24 10:49:23 +0100

    Make async calls to pair update
    As far as I understand it the public Kraken trades API is rate limited
    per pair. So it should be possible to make calls in parallel. Hence,
    threading support has been added to the update script.

--- datums_warehouse/scripts/update.py
+++ datums_warehouse/scripts/update.py
@@ -1,3 +1,17 @@
-def update_pairs(warehouse, pairs):
-    for p in pairs:
-        warehouse.update(p)
+from threading import Thread
+
+from datums_warehouse.db import make_warehouse
+
+
+def update_pairs(cfg, pairs):
+    def update_pair(wh_cfg, pair):
+        wh = make_warehouse(wh_cfg)
+        wh.update(pair)
+
+    processes = [Thread(target=update_pair, name=f"process: {p}", args=(cfg, p)) for p in pairs]
+
+    for prc in processes:
+        prc.start()
+
+    for prc in processes:
+        prc.join()
--- tests/scripts/test_update_pairs.py
+++ tests/scripts/test_update_pairs.py
@@ -5,13 +5,13 @@ from datums_warehouse.scripts.update import update_pairs
 
 class WarehouseSpy:
     def __init__(self):
-        self.received_pair = None
+        self.received_pairs = []
 
     def update(self, pair):
         import time
         import random
         time.sleep(random.uniform(0.01, 0.1))
-        self.received_pair = pair
+        self.received_pairs.append(pair)
 
 
 @pytest.fixture
@@ -19,11 +19,18 @@ def warehouse():
     return WarehouseSpy()
 
 
+@pytest.fixture(autouse=True)
+def inject_warehouse(monkeypatch, warehouse):
+    import datums_warehouse.scripts.update as mut
+    monkeypatch.setattr(mut, 'make_warehouse', lambda cfg: warehouse)
+    return warehouse
+
+
 def test_update_single_pair(warehouse):
     update_pairs(warehouse, ['pair'])
-    assert warehouse.received_pair == 'pair'
+    assert warehouse.received_pairs == ['pair']
 
 
 def test_update_multiple_pairs(warehouse):
-    update_pairs(warehouse, ['pair'])
-    assert warehouse.received_pair == 'pair'
+    update_pairs(warehouse, ['A', 'B'])
+    assert {'A', 'B'} == set(warehouse.received_pairs)
